8ec6bbc37a26dca5b338ae30be7c9e0b72407507
medical-equipment <46204348+medical-equipment@users.noreply.github.com>
2018-12-27 23:31:38 +0000

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+var app_firebase = {}
+(function () {
+var config = {
+   apiKey: "AIzaSyAXG4Wm8Tf6R4ykqBpyOaJ9ei5JHP8ahTA",
+   authDomain: "med-tools.firebaseapp.com",
+   databaseURL: "https://med-tools.firebaseio.com",
+   projectId: "med-tools",
+   storageBucket: "med-tools.appspot.com",
+   messagingSenderId: "764014078931"
+ };
+   
+firebase.initializeApp(config);
+app_firebase = firebase;
+ 
+})()
